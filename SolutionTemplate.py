6063d8584fe92fe2015c8f208373e2c6977c1968
hossg <hossein.ghodse@gmail.com>
2018-04-04 10:42:18 +0100

    Updated solution template to include basic timing and system information

--- SolutionTemplate.py
+++ SolutionTemplate.py
@@ -3,19 +3,51 @@
 # put the expected answer here
 expectedAnswer=123456789
 
-import logging, math
+import logging, math, timeit, time, psutil, platform
+
+# Utility function for measuring the performance of solutions
+processtime=0.0
+walltime=0.0
+def stopwatch():
+    global walltime, processtime
+    wt=time.time()
+    ct=time.clock()
+    wtElapsed=wt-walltime
+    ctElapsed=ct-processtime
+    walltime=wt
+    processtime=ct
+    return('Elapsed process time:{}s, Elapsed clock time:{}s'.format(ctElapsed,wtElapsed))
+
+def getsysteminfo():
+    p=platform.platform()+' ' +platform.processor()+' Python: '+platform.python_version()
+    memory=psutil.virtual_memory()
+    cpuc=psutil.cpu_count()
+    cpup=psutil.cpu_count(logical=True)
+    cpuf=psutil.cpu_freq()
+    cput=psutil.cpu_times_percent(percpu=False)
+
+    return 'Platform: {}, Memory: {} Physical CPUs: {}, Logical CPUs: {}, Frequency (MHz): {}, Utilisation: {}'.format\
+        (p,memory,cpuc,cpup,cpuf,cput)
+
 
 def solution():
     # just a placeholder for where the solution to the problem will be stored and then returned
     solution=987654321
 
     #implement solution to the problem here
+    time.sleep(1)
+
+
 
     return solution
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s %(message)s')
+    stopwatch() #start timing
     solution = solution()
-    assert (solution == expectedAnswer)
+    timetaken=stopwatch() #stop timing
+    #assert (solution == expectedAnswer)
     logging.info('Solution = {}'.format(solution))
+    logging.info(timetaken)
+    logging.info('System info: {}'.format(getsysteminfo()))
