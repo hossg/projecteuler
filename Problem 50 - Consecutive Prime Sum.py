06aa4fbab958a1feaf890d3dcac59a8cd1c1c22b
hossg <hossein.ghodse@gmail.com>
2018-04-06 14:03:07 +0100

    Solved problem 50

--- Problem 50 - Consecutive Prime Sum.py
+++ Problem 50 - Consecutive Prime Sum.py
@@ -8,9 +8,9 @@
 # Which prime, below one-million, can be written as the sum of the most consecutive primes?
 
 # put the expected answer here
-expectedAnswer=123456789
+expectedAnswer=997651
 
-import logging, math, timeit, time, psutil, platform
+import logging, math, timeit, time, psutil, platform, os
 
 # Utility function for measuring the performance of solutions
 processtime=0.0
@@ -58,38 +58,56 @@ def sieve(upperlimit, onlyPrimes=False):
 
 
 def solution():
-    # just a placeholder for where the solution to the problem will be stored and then returned
-    solution=987654321
 
-    isPrimes = sieve(1001)
-    primes = sieve(1001,onlyPrimes=True)
+    # we will need both a list of primes, and a quick lookup to see whether a number IS prime
+    isPrimes = sieve(1000001,onlyPrimes=False)
+    primes = sieve(1000001,onlyPrimes=True)
 
-    longestSequenceStartingPrime=0
-    totalOfLongestSequence=0
-    for i,p in enumerate(primes):
-        total=p
-        previoustotal=p
-        currentSequenceLength=1
-        for nextp in primes[i+1:]:
-            total+=nextp
+    # for each prime number, build a sequence of the sums of it and all # subsequent primes, and store that sequence
+    # in a dict keyed off the starting prime number
+    primeSums={}
+    for i,startingPrime in enumerate(primes):
+        sequenceSums=[0]
+        for prime in primes[i:]:        # start adding primes, for all the primes starting with the starting prime
+            sum=sequenceSums[-1]+prime  # take the previous sum, and add the next prime
+            if sum>=len(isPrimes)+2:    # we can stop as soon as we get to the limit of the range we're interested in
+                break                   # +2 is just because the list of isPrimes *starts* with 2 in the zeroth slot
+            sequenceSums.append(sequenceSums[-1]+prime) # otherwise, add the new running total to the sequence
+        primeSums[startingPrime]=sequenceSums   # and when we're done, store the entire sequence in the dict, keyed off
+                                                # the starting prime
 
-            if total <1000 and isPrimes[total-2]!=0:
-                currentSequenceLength+=1
-                previoustotal=total
-            else:
-                break
-        if currentSequenceLength>longestSequenceStartingPrime:
-            longestSequenceStartingPrime=p
-            totalOfLongestSequence=previoustotal
-    logging.info('Sequence starts with {} and totals to {}'.format(longestSequenceStartingPrime,totalOfLongestSequence))
+    # now we're going to look at each sequence of sums we just generated, and for each one work backwards from the end
+    # until we find the first prime number (from the end). This item will meet our broad criteria: it is a prime, and
+    # it's the sum of a number of primes, starting with the starting prime for that sequence.
+    maxP=1
+    maxSequence=[]
+    for p in primeSums:                                         # for each starting prime
+        for i, e in reversed(list(enumerate(primeSums[p]))):    # 'e' will be the sum of primes, and i will be the index
+                                                                # of that item from the start of the sequence, even
+                                                                # though we are using 'reverse' to work from the back of
+                                                                # the list.
 
+            if e<len(isPrimes) and isPrimes[e-2]!=0:            # Allowing for +2 indexing, is this item prime?
+                if i>maxP:                                      # If so, and if the index is further from the start than
+                    maxP=i                                      # the greatest index found for other starting primes,
+                                                                # then we have a new candidate solution.
 
+                    maxSequence=primeSums[p][1:i+1]             # Since we're here, let's grab the actual sums for that
+                                                                # solution
+
+    solution = maxSequence[-1]                                  # The final sum of the longest sequence IS the prime
+    i = primes.index(maxSequence[0])                            # Now let's also get the actual primes that combine
+    primeSequence = [p for p in primes[i:i + len(maxSequence)]] # to give us that largest prime sequence total.
+
+    logging.info('Prime {} is the sum of {} primes starting at {}, has items: {}.'.format(solution, len(primeSequence),
+                                                                                          maxSequence[0],primeSequence))
 
     return solution
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s %(message)s')
+    logging=logging.getLogger(os.path.basename(__file__))
     stopwatch() #start timing
     solution = solution()
     timetaken=stopwatch() #stop timing
