f52fb841053827ebe099b9b486e15d3266f00edf
hossg <hossg@users.noreply.github.com>
2023-01-16 10:25:01 +0000

    Copied a solution to P64 into here.
    Found on a non-repo version of the folder, so not sure if it works, but didn't want to lose the work I'd done!

---
+++ Problem 64 - Odd Period Square Roots.py
@@ -0,0 +1,53 @@
+# put the expected answer here
+expectedAnswer=123456789
+
+import logging, math, timeit, time, psutil, platform, os
+
+
+def solution():
+    # just a placeholder for where the solution to the problem will be stored and then returned
+
+
+    solution=987654321
+    return solution
+
+
+def get_a(n,a):
+    next_a = math.floor(math.sqrt(n))
+    answer = next_a + (1/(1/(math.sqrt(n) - next_a))
+    answer = next_a + (1/(math.sqrt(n)+next_a)/(n+next_a^2)
+
+# Utility function for measuring the performance of solutions
+processtime=0.0
+walltime=0.0
+def stopwatch():
+    global walltime, processtime
+    wt=time.time()
+    ct=time.clock()
+    wtElapsed=wt-walltime
+    ctElapsed=ct-processtime
+    walltime=wt
+    processtime=ct
+    return('Elapsed process time:{}s, Elapsed clock time:{}s'.format(ctElapsed,wtElapsed))
+
+def getsysteminfo():
+    p=platform.platform()+' ' +platform.processor()+' Python: '+platform.python_version()
+    memory=psutil.virtual_memory()
+    cpuc=psutil.cpu_count()
+    cpup=psutil.cpu_count(logical=True)
+    cpuf=psutil.cpu_freq()
+    cput=psutil.cpu_times_percent(percpu=False)
+
+    return 'Platform: {}, Memory: {} Physical CPUs: {}, Logical CPUs: {}, Frequency (MHz): {}, Utilisation: {}'.format\
+        (p,memory,cpuc,cpup,cpuf,cput)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s %(message)s')
+    logging=logging.getLogger(os.path.basename(__file__))
+    stopwatch() #start timing
+    solution = solution()
+    timetaken=stopwatch() #stop timing
+    #assert (solution == expectedAnswer)
+    logging.info('Solution = {}'.format(solution))
+    logging.info(timetaken)
+    logging.info('System info: {}'.format(getsysteminfo()))
