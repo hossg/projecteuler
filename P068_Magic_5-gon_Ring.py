a7e9ff3b1eeab6d68e71909bfa59946b36a903e5
hossg <hossg@users.noreply.github.com>
2024-10-19 15:25:38 +0100

    Update P068_Magic_5-gon_Ring.py

--- P068_Magic_5-gon_Ring.py
+++ P068_Magic_5-gon_Ring.py
@@ -1,103 +1,74 @@
-# Put the problem description here, including a link to the problem on the website if available.
+# https://projecteuler.net/problem=68
+
+# Ultimately we are looking for the biggest (concatenated/lexicographical) answers; for that reason we want to have the
+# largest numbers on the outside, and the smaller numbers on the inside.
+#
+# So for a ring size of 3, with 6 digits, we want the numbers 1-3 on the inner ring, and the numbers 4-6 on the outer ring;
+# for a ring size of 5, with 10 numbers, we want 1-5 on the inside and 6-10 on the outside.  Lexicographically, we also know
+# that we want the two digit number 10 on the outside ring, so it doesn't get counted twice in the ring identification, pushing
+# the result to 17 digits, rather than the 10 we are looking for.
+#
+# So we will generate permutations of the digits for the internal ring, and permutations of the digits for the external ring.
+# For each of these we will test to see if it's a magic ring or not (totals are all the same) and if it is we will
+# concatenate the ring definition, and see if it has a maximum value.
+
 
 # Put the expected answer here
-expectedAnswer=123456789
+expectedAnswer=6531031914842725
 
 import logging,os, itertools
 
 
-def ring3():
-    numbers=list(range(1,7))*2 # because each number can be used at most twice, if in the inner ring
-    permutation_length=9
-    possibilities = (itertools.permutations(numbers,permutation_length))
-    maxRingVal=0
-    for i in possibilities:
-        if isValid3Ring(i):
-            ringVal = concatRing(i)
-            logging.debug(f'{sum(i[0:3])}: {ringVal}')
-            if ringVal > maxRingVal:
-                maxRingVal = ringVal
-    logging.debug(f'Max Ring Value: {maxRingVal}')  
-    return maxRingVal
 
-def ring5():
-    numbers=list(range(1,11))*2 # because each number can be used at most twice, if in the inner ring
-    permutation_length=15
-    possibilities = set(itertools.permutations(numbers,permutation_length))
-    maxRingVal=0
-    for i in possibilities:
-        if isValid5Ring(i):
-            ringVal = concatRing(i)
-            logging.debug(f'{sum(i[0:3])}: {ringVal}')
-            if ringVal > maxRingVal:
-                maxRingVal = ringVal
-    logging.debug(f'Max Ring Value: {maxRingVal}')  
-    return maxRingVal
+def is_magic_ring(internal, external, ring_size):
+    # Calculate the sum of each triplet and check if all sums are equal
+    target_sum = external[0] + internal[0] + internal[1]
+    for i in range(1, ring_size):
+        if external[i] + internal[i] + internal[(i + 1) % ring_size] != target_sum:
+            return False
+    return True
 
-def concatRing(r):
-    s=''
-    for n in r:
-        s += str(n)
-    return int(s)
-    
-def isValid3Ring(ring):   
-    if ring[1]!=ring[8]: return False
-    if ring[2]!=ring[4]: return False
-    if ring[5]!=ring[7]: return False
+def max_concatenation(internal, external, ring_size):
+    # Find the line with the smallest external node to start the string
+    start = external.index(min(external))
+    result = []
+    for i in range(ring_size):
+        index = (start + i) % ring_size
+        result.append(f"{external[index]}{internal[index]}{internal[(index + 1) % ring_size]}")
+    return ''.join(result)
 
-    if ring[0]>ring[3]: return False
-    if ring[0]>ring[6]: return False
-        
-    total = sum(ring[0:3])
-    total2 = sum(ring[3:6])
-    total3 = sum(ring[6:9])
-    if total==total2 ==total3:
-        return True
-    else:
-        return False
+def solve5():
+    max_solution = 0
+    # External nodes are 6-10, internal nodes are 1-5
+    for internal in itertools.permutations(range(1, 6)):
+        for external in itertools.permutations(range(6, 11)):
+            if is_magic_ring(internal, external,5):
+                concat = max_concatenation(internal, external,5)
+                if len(concat) == 16:
+                    max_solution = max(max_solution, int(concat))
+    return max_solution
+
+def solve(ring_size):
+    max_solution = 0
+    # External nodes are 6-10, internal nodes are 1-5
+    for internal in itertools.permutations(range(1, ring_size+1)):
+        for external in itertools.permutations(range(ring_size+1, ring_size*2+1)):
+            if is_magic_ring(internal, external,ring_size  ):
+                concat = max_concatenation(internal, external,ring_size)
+                # if len(concat) == 16:  # Ensuring it's a 16-digit solution, but perhaps redundant since 10 is definitely only counted once on the external ring
+                max_solution = max(max_solution, int(concat))
+    return max_solution
 
 
-def isValid5Ring(ring):   
-    if ring[1]!=ring[14]: return False
-    if ring[2]!=ring[4]: return False
-    if ring[5]!=ring[7]: return False
-    if ring[8]!=ring[10]: return False
-    if ring[11]!=ring[13]: return False
-    
-    if ring[0]>ring[3]: return False
-    if ring[0]>ring[6]: return False
-    if ring[0]>ring[9]: return False
-    if ring[0]>ring[12]: return False
-
-    if ring[0] != 10 and ring[3]!=10 and ring[6]!=10 and ring[9]!=10 and ring[12]!=10: return False
-
-    total = sum(ring[0:3])
-    total2 = sum(ring[3:6])
-    total3 = sum(ring[6:9])
-    total4 = sum(ring[9:12])
-    total5 = sum(ring[12:15])
-    if total==total2 ==total3==total4==total5:
-        return True
-    else:
-        return False
-    
-# observation: in a 5-gon made of 5 strings of 3 numbers, you need 10 numbers
-# if the number 10 is in an outer most node, then the string that defines the ring will be 16 characters long
-# if the number 10 is in an inner node, the defining string will be 17 characters long as the 10 will be used twice
-# we are looking only for 16 digit ring definitions, and ring definitions start with the lowest external number
-# and so our answer will not start with '10'
-
-# This function calculates the answer and returns it
 def solution():
-    #implement solution to the problem here
-    solution=ring3()
-    solution=ring5()
-    return solution
+    return solve(5)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(message)s')
     logging=logging.getLogger(os.path.basename(__file__))
+    logging.debug(solve(3))
     solution = solution()
-    # logging.info('Solution = {}'.format(solution))
-    # assert (solution == expectedAnswer)
+    logging.info('Solution = {}'.format(solution))
+    assert (solution == expectedAnswer)
     
