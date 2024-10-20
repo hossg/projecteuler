c33b441780ab5faeef04c274405fc9ce61cd77ba
hossg <hossg@users.noreply.github.com>
2024-10-20 11:30:00 +0100

    Implemented a generic solution to P5

--- P005_Smallest_Multiple.py
+++ P005_Smallest_Multiple.py
@@ -7,26 +7,33 @@
 # to form them as a composite number to make sure we have "enough" of each prime
 # specifically we need 4 2's to get 16, and 2 3's to get 9
 
-# 2,3,5,7,11,13,17,19 ?
+
 expectedAnswer = 232792560
 import os
 import logging
+import math
 
 logger = logging.getLogger(os.path.basename(__file__))
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)
 
-#TODO - need to implement a generic routine here
+def lcm_multiple(numbers):
+    result = numbers[0]
+    for number in numbers[1:]:
+        result = lcm(result, number)
+    return result
+
 def solution():
     x = 2 * 3 * 5 * 7 * 11 * 13 * 17 * 19 * 2 * 2 * 2 * 3
-
-    return x
+    numbers = range(1, 21)
+    result = lcm_multiple(numbers)
+    return result
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(message)s')
-    logging=logging.getLogger(os.path.basename(__file__))
     solution = solution()
     logging.info('Solution = {}'.format(solution))
     assert (solution == expectedAnswer)
